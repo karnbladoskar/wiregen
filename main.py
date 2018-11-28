1b7191df45cc9c02f67d02af7dcc7c7df4018842
Oskar Karnblad <oskar.karnblad@volvocars.com>
2018-11-28 14:30:15 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+train_path = "/data/training"
+test_path = "data/testing"
+
+
+def load_test(test_path):
+
+    return 1
+
+
+def load_training(train_path):
+
+    return 1
