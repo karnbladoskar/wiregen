6b95c0ba21048c031ae08c06e15995f2490891e7
oskarkarnblad <oskar.karnblad@gmail.com>
2018-11-28 19:00:51 +0100

    Running structual similarity

--- scripts/compare/structuralsimilarity.py
+++ scripts/compare/structuralsimilarity.py
@@ -1,69 +1,44 @@
-#from skimage.measure import structural_similarity as ssim
+from skimage.measure import compare_ssim as ssim
 import matplotlib.pyplot as plt
-import numpy as np
 import cv2
 
-def mse(imageA, imageB):
-	# the 'Mean Squared Error' between the two images is the
-	# sum of the squared difference between the two images;
-	# NOTE: the two images must have the same dimension
-	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-	err /= float(imageA.shape[0] * imageA.shape[1])
-	
-	# return the MSE, the lower the error, the more "similar"
-	# the two images are
-	return err
 
-def compare_images(imageA, imageB, title):
-	# compute the mean squared error and structural similarity
-	# index for the images
-	m = mse(imageA, imageB)
-	s = ssim(imageA, imageB)
+class CompareImages:
 
-	# setup the figure
-	fig = plt.figure(title)
-	plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
+    def __init__(self, SRC, TRG, WIDTH, HEIGHT, METHOD):
+		self.WIDTH = WIDTH
+		self.HEIGHT = HEIGHT
+		self.METHOD = METHOD
+		self.SRC = SRC
+		self.TRG = TRG
+		self.compare_all_images()
 
-	# show first image
-	ax = fig.add_subplot(1, 2, 1)
-	plt.imshow(imageA, cmap = plt.cm.gray)
-	plt.axis("off")
+	def compare_images(self, image1, image2, method):
+		if self.method == "FAISS":
+			return 1
 
-	# show the second image
-	ax = fig.add_subplot(1, 2, 2)
-	plt.imshow(imageB, cmap = plt.cm.gray)
-	plt.axis("off")
+		elif self.method == "SSIM":
+			similarity = ssim(image1, image2)
+			print("SSIM: %.2f" % similarity)
+			return similarity
 
-	# show the images
-	plt.show()
 
-# load the images -- the original, the original + contrast,
-# and the original + photoshop
-original = cv2.imread("images/jp_gates_original.png")
-contrast = cv2.imread("images/jp_gates_contrast.png")
-shopped = cv2.imread("images/jp_gates_photoshopped.png")
+	def compare_all_images(self, source_img, target_imgs):
+		source_img = cv2.resize(source_img, (WIDTH, HEIGHT))
 
-# convert the images to grayscale
-original = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
-contrast = cv2.cvtColor(contrast, cv2.COLOR_BGR2GRAY)
-shopped = cv2.cvtColor(shopped, cv2.COLOR_BGR2GRAY)
+		target_probs = []
+		for target_img in target_imgs:
+			target_img = cv2.resize(target_img, (self.WIDTH, self.HEIGHT))
+			self.compare_images(source_img, target_img, self.METHOD)
 
-# initialize the figure
-fig = plt.figure("Images")
-images = ("Original", original), ("Contrast", contrast), ("Photoshopped", shopped)
+		return target_probs
 
-# loop over the images
-for (i, (name, image)) in enumerate(images):
-	# show the image
-	ax = fig.add_subplot(1, 3, i + 1)
-	ax.set_title(name)
-	plt.imshow(image, cmap = plt.cm.gray)
-	plt.axis("off")
+if __name__ == "__main__":
+	WIDTH = 512
+	HEIGHT = 512
+	METHOD = "SSIM"
 
-# show the figure
-plt.show()
+	SRC = cv2.imread("../../data/source/add_block.jpg", 0)
+	TRG = cv2.imread("../../data/target/add_block.png", 0)
 
-# compare the images
-compare_images(original, original, "Original vs. Original")
-compare_images(original, contrast, "Original vs. Contrast")
-compare_images(original, shopped, "Original vs. Photoshopped")
+	CompareImages(SRC, TRG, WIDTH, HEIGHT, METHOD)
